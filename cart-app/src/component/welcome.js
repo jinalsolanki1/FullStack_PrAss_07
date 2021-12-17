bcf8f7445918ed25eb6c5ad14b29eff5eaaa4081
jinalsolanki1 <91380043+jinalsolanki1@users.noreply.github.com>
2021-12-17 23:11:16 +0530

    Add files via upload

---
+++ cart-app/src/component/welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Welcome = () => {
+    return(
+    <center>
+        <h1>Welcome To Food Cart</h1>
+      <br/>
+      <h2><a href="/cart">Click Here For Add Item To Cart</a></h2>
+    </center>
+    )
+};
+export default Welcome;
